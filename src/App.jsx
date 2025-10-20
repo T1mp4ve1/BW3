320a3f624e6ffdf933613046bc33594379c7842b
RiccardoEpicode <realiriccardo05@gmail.com>
2025-10-20 17:27:02 +0200

    update merge work

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import { Container, Row, Col } from "react-bootstrap";
 import "./App.css";
 import Asidex from "./components/Asidex";
 import ProfileMain from "./components/ProfileMain";
+import NavBarLinkIn from "./components/NavBarLinkIn";
 
 function App() {
   return (
     <>
+      <NavBarLinkIn />
       <Container>
         <Row>
           <Col className="col-sm-8">
