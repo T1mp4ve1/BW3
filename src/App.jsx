2fee9410efa2915f390076ba6dee7ec99f9d3a65
T1mp4ve1 <pavelwire@outlook.it>
2025-10-21 12:56:27 +0200

    asedex

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,10 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import NavBarLinkIn from "./components/NavBarLinkIn";
-import { Container } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 import FooterLinkIn from "./components/FooterLinkIn";
 import ProfileMain from "./components/ProfileMain";
+import Asidex from "./components/Asidex";
 
 function App() {
   return (
@@ -14,7 +15,19 @@ function App() {
         <NavBarLinkIn />
         <Container>
           <Routes>
-            <Route path="/" element={<ProfileMain />} />
+            <Route
+              path="/"
+              element={
+                <Row>
+                  <Col xs={8} className="d-flex justify-content-center">
+                    <ProfileMain />
+                  </Col>
+                  <Col xs={4} className="d-flex justify-content-center">
+                    <Asidex />
+                  </Col>
+                </Row>
+              }
+            />
           </Routes>
         </Container>
         <FooterLinkIn />
