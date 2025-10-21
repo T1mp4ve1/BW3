06dd34e72db72d60cc537cc1a091e63abe0d7d88
T1mp4ve1 <pavelwire@outlook.it>
2025-10-21 12:43:02 +0200

    mainProf

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
 import "./App.css";
-import { BrowserRouter, Routes } from "react-router";
+import { BrowserRouter, Route, Routes } from "react-router";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import NavBarLinkIn from "./components/NavBarLinkIn";
 import { Container } from "react-bootstrap";
 import FooterLinkIn from "./components/FooterLinkIn";
+import ProfileMain from "./components/ProfileMain";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <NavBarLinkIn />
-        <Routes></Routes>
+        <Container>
+          <Routes>
+            <Route path="/" element={<ProfileMain />} />
+          </Routes>
+        </Container>
         <FooterLinkIn />
       </BrowserRouter>
     </>
