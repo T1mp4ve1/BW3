be11e0430b9a12f171ed8baeb75bcaee42aa363c
T1mp4ve1 <pavelwire@outlook.it>
2025-10-21 17:07:28 +0200

    drop aziende

--- src/components/NavBarLinkIn.jsx
+++ src/components/NavBarLinkIn.jsx
@@ -7,6 +7,7 @@ import {
   Form,
   Row,
   Col,
+  Card,
 } from "react-bootstrap";
 import { Link } from "react-router";
 import "./css/NavBarLinkIn.css";
@@ -47,7 +48,7 @@ const NavBarLinkIn = () => {
             lg={6}
             className="d-flex  justify-content-end align-items-center"
           >
-            <Nav className="d-flex flex-row align-items-center gap-1 gap-md-3 gap-lg-4">
+            <Nav className="d-flex flex-row justify-content-around align-items-center">
               <Link
                 to={"/"}
                 className="d-flex flex-column align-items-center mx-1"
@@ -71,6 +72,7 @@ const NavBarLinkIn = () => {
                 <i className="bi bi-bell-fill fs-5"></i>
                 <p className="m-0 d-none d-lg-block">Notifiche</p>
               </Link>
+
               {/* dropdown Tu */}
               <div className="d-flex flex-column align-items-center mx-1">
                 <div>
@@ -81,11 +83,12 @@ const NavBarLinkIn = () => {
                   />
                 </div>
                 <NavDropdown
+                  align={"end"}
                   title={<span className="d-none d-lg-inline">Tu</span>}
                   id="navbarScrollingDropdown"
                 >
                   <div
-                    className="p-2 dropDownMenu"
+                    className="p-3 dropDownMenu"
                     href="#action1"
                     style={{
                       width: "300px",
@@ -124,7 +127,14 @@ const NavBarLinkIn = () => {
                           variant="outline-primary"
                           className="rounded-5 w-50 mt-1"
                         >
-                          Visualiza profilo
+                          <p
+                            style={{
+                              fontSize: 14,
+                              fontWeight: 600,
+                            }}
+                          >
+                            Visualiza profilo
+                          </p>
                         </Button>
                       </Link>
                     </div>
@@ -200,38 +210,147 @@ const NavBarLinkIn = () => {
                 <i className="bi bi-grid-3x3-gap-fill fs-5"></i>
               </div>
               <NavDropdown
+                align={"end"}
                 title={
                   <span className="d-none d-lg-inline">Per le aziende</span>
                 }
                 id="navbarScrollingDropdown"
               >
-                <NavDropdown.Item href="#action1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action3">
-                  Something else here
-                </NavDropdown.Item>
+                <Card
+                  className="d-flex flex-row justify-content-between p-4 border-0"
+                  style={{
+                    width: "650px",
+                  }}
+                >
+                  <div>
+                    <h5 className="mb-3">Le mie app</h5>
+                    <div
+                      className="d-flex flex-column ms-2"
+                      id="aziendeCardLeft"
+                    >
+                      <a
+                        href="/"
+                        onClick={(e) => e.preventDefault()}
+                        className="d-flex align-items-center mb-3"
+                      >
+                        <i class="bi bi-compass-fill text-primary fs-4 me-2"></i>
+                        <p>Vendi</p>
+                      </a>
+                      <a
+                        href="/"
+                        onClick={(e) => e.preventDefault()}
+                        className="d-flex align-items-center mb-3"
+                      >
+                        <i class="bi bi-microsoft-teams text-primary fs-4 me-2"></i>
+                        <p>Gruppi</p>
+                      </a>
+                      <p className="text-secondary talent mt-2">Talent</p>
+                      <a
+                        href="/"
+                        onClick={(e) => e.preventDefault()}
+                        className="d-flex align-items-center mb-3"
+                      >
+                        <i class="bi bi-person-fill-check text-primary fs-4 me-2"></i>
+                        <p>Talent Insights</p>
+                      </a>
+                      <a
+                        href="/"
+                        onClick={(e) => e.preventDefault()}
+                        className="d-flex align-items-center mb-3"
+                      >
+                        <i class="bi bi-bag-plus-fill text-primary fs-4 me-2"></i>
+                        <p>Pubblica un’offerta di lavoro</p>
+                      </a>
+                      <p className="text-secondary talent mt-2">Vendite</p>
+                      <a
+                        href="/"
+                        onClick={(e) => e.preventDefault()}
+                        className="d-flex align-items-center mb-3"
+                      >
+                        <i class="bi bi-patch-check-fill text-primary fs-4 me-2"></i>
+                        <p>Marketplace dei servizi</p>
+                      </a>
+
+                      <p className="text-secondary talent mt-2">Marketing</p>
+                      <a
+                        href="/"
+                        onClick={(e) => e.preventDefault()}
+                        className="d-flex align-items-center mb-3"
+                      >
+                        <i class="bi bi-bullseye text-primary fs-4 me-2"></i>
+                        <p>Pubblicizza</p>
+                      </a>
+
+                      <p className="text-secondary talent mt-2">Learning</p>
+                      <a
+                        href="/"
+                        onClick={(e) => e.preventDefault()}
+                        className="d-flex align-items-center mb-3"
+                      >
+                        <i class="bi bi-play-btn-fill text-primary fs-4 me-2"></i>
+                        <p>Learning</p>
+                      </a>
+                    </div>
+                  </div>
+
+                  <div className="border-start ps-5" id="aziendeCardRight">
+                    <h5 className="mb-3">Scopri altro per il business</h5>
+                    <a href="/" onClick={(e) => e.preventDefault()}>
+                      <p className="p1">Assumi su LinkedIn</p>
+                      <p className="mb-3">Trova, attrai e assumi</p>
+                    </a>
+
+                    <a href="/" onClick={(e) => e.preventDefault()}>
+                      <p className="p1">Vendi con LinkedIn</p>
+                      <p className="mb-3">
+                        Sblocca nuove opportunità di vendita
+                      </p>
+                    </a>
+
+                    <a href="/" onClick={(e) => e.preventDefault()}>
+                      <p className="p1">Offerta di lavoro gratuita</p>
+                      <p className="mb-3">
+                        Ottieni rapidamente candidati qualificati
+                      </p>
+                    </a>
+
+                    <a href="/" onClick={(e) => e.preventDefault()}>
+                      <p className="p1">Fai pubblicità su LinkedIn</p>
+                      <p className="mb-3">
+                        Acquisisci clienti e fai crescere la tua azienda
+                      </p>
+                    </a>
+
+                    <a href="/" onClick={(e) => e.preventDefault()}>
+                      <p className="p1">Inizia con Premium</p>
+                      <p className="mb-3">Amplia e sfrutta la tua rete</p>
+                    </a>
+
+                    <a href="/" onClick={(e) => e.preventDefault()}>
+                      <p className="p1">Impara con LinkedIn</p>
+                      <p className="mb-3">
+                        Corsi per formare i tuoi dipendenti
+                      </p>
+                    </a>
+
+                    <a href="/" onClick={(e) => e.preventDefault()}>
+                      <p className="p1">Centro per amministratori</p>
+                      <p className="mb-3">
+                        Gestisci i dettagli di fatturazione e account
+                      </p>
+                    </a>
+                    <a
+                      href="/"
+                      onClick={(e) => e.preventDefault()}
+                      className="d-flex align-items-center"
+                    >
+                      <p className="p1">Crea una pagina aziendale </p>
+                      <i class="bi bi-plus p1 fs-4"></i>
+                    </a>
+                  </div>
+                </Card>
               </NavDropdown>
             </div>
-            {/* <a
-              className="text-center"
-              href="/"
-              onClick={(e) => e.preventDefault()}
-              style={{
-                color: "#9e6427ff",
-                textDecoration: "underline",
-                fontSize: 12,
-                display: "-webkit-box",
-                WebkitLineClamp: 2,
-                WebkitBoxOrient: "vertical",
-                overflow: "hidden",
-                textOverflow: "ellipsis",
-              }}
-            >
-              Prova Premium per 0 EUR
-            </a> */}
           </Col>
         </Row>
       </Container>
