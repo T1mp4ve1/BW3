d6b3657de9f3f0e900ca18e4af33048cfe72a714
RiccardoEpicode <realiriccardo05@gmail.com>
2025-10-24 15:47:04 +0200

    main reset

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,7 @@
-import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap-icons/font/bootstrap-icons.css";
-import NavBarLinkIn from "./components/NavBarLinkIn";
-import { Col, Container, Row } from "react-bootstrap";
-import FooterLinkIn from "./components/FooterLinkIn";
-import ProfileMain from "./components/pages/ProfileMain";
-import Asidex from "./components/Asidex";
-import HomeLinkIn from "./components/pages/HomeLinkIn";
-import SearchPage from "./components/pages/SearchPage";
+import React from "react";
 
 function App() {
-  return (
-    <>
-      <BrowserRouter>
-        <NavBarLinkIn />
-        <Container className="d-flex justify-content-center mt-5">
-          <Routes>
-            <Route path="/" element={<HomeLinkIn />} />
-            <Route
-              path="/profile"
-              element={
-                <Row className="d-flex justify-content-center">
-                  <Col xs={8} className="p-0">
-                    <ProfileMain />
-                  </Col>
-                  <Col xs={4} className="p-0">
-                    <Asidex />
-                  </Col>
-                </Row>
-              }
-            />
-            <Route path="/search/:query" element={<SearchPage />} />
-          </Routes>
-        </Container>
-        <FooterLinkIn />
-      </BrowserRouter>
-    </>
-  );
+  return <div>ciao</div>;
 }
 
 export default App;
